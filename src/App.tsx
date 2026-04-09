3cb315989d7f1278a9d94beb1728d3a98b1e514e
Otownparty <otownparty@outlook.com>
2026-04-09 22:39:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/otown-culture-beats/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/events" element={<Events />} />
