cc87a802b6c4458905aa7fe863c96f51d5c0c7af
Otownparty <otownparty@outlook.com>
2026-04-30 22:49:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import StaffAuth from "./pages/StaffAuth";
 import NotFound from "./pages/NotFound";
 import Success from "./pages/Success";
 import Record from "./pages/Record";
+import Vendor from "./pages/Vendor";
 
 const queryClient = new QueryClient();
 
@@ -34,6 +35,7 @@ const App = () => (
           <Route path="/staff" element={<StaffAuth />} />
           <Route path="/success" element={<Success />} />
           <Route path="/record" element={<Record />} />
+          <Route path="/vendor" element={<Vendor />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
